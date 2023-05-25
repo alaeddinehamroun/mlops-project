57f25cdb98f3049931137520a8a9f7e2f7ed2bbb
alaeddinehamroun <alaeddinehamroun@insat.u-carthage.tn>
2023-05-25 15:20:42 +0100

    restructured the repo

---
+++ src/metrics.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+
+
+# Custom metric functions
+def precision(y_true, y_pred):
+    true_positives = tf.keras.backend.sum(tf.keras.backend.round(tf.keras.backend.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = tf.keras.backend.sum(tf.keras.backend.round(tf.keras.backend.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + tf.keras.backend.epsilon())
+    return precision
+
+def recall(y_true, y_pred):
+    true_positives = tf.keras.backend.sum(tf.keras.backend.round(tf.keras.backend.clip(y_true * y_pred, 0, 1)))
+    possible_positives = tf.keras.backend.sum(tf.keras.backend.round(tf.keras.backend.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + tf.keras.backend.epsilon())
+    return recall
+
+def f1_score(y_true, y_pred):
+    p = precision(y_true, y_pred)
+    r = recall(y_true, y_pred)
+    f1_score = 2 * ((p * r) / (p + r + tf.keras.backend.epsilon()))
+    return f1_score
--- src/train.py
+++ src/train.py
@@ -1,18 +1,143 @@
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '0' 
+import matplotlib.pyplot as plt
+import mlflow
+from mlflow.models.signature import infer_signature
+import tensorflow as tf
 
-from data_preprocessing import train_val_generators
-from model import create_model
-
-# Load & preprocess the dataset
-trainging_dir = 'data/training'
-validation_dir = 'data/validation'
-train_generator, validation_generator = train_val_generators(training_dir=trainging_dir, validation_dir=validation_dir)
-
-model = create_model()
+from metrics import precision, recall, f1_score
+mlflow.tensorflow.autolog()
 
 
-history = model.fit(train_generator,
-                    epochs=2,
+
+
+
+#os.environ['CUDA_VISIBLE_DEVICES'] = '0' 
+data_dir = 'data/training'
+batch_size = 32
+img_height = 180
+img_width = 180
+
+
+
+with mlflow.start_run() as run:
+
+    # Load dataset
+    train_ds = tf.keras.utils.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
+
+    val_ds = tf.keras.utils.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
+    
+
+    # Get number of classes
+    num_classes = len(train_ds.class_names)
+
+    # Configure the dataset for performance
+    # AUTOTUNE = tf.data.AUTOTUNE
+    # train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    # val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)    
+
+    # Data preprocessing
+
+    # Normalization
+    normalization_layer = tf.keras.layers.Rescaling(1./255)
+
+    # 
+    
+
+    # Data augmentation
+    data_augmentation = tf.keras.Sequential([
+        tf.keras.layers.RandomFlip("horizontal_and_vertical"),
+        tf.keras.layers.RandomRotation(0.2),
+    ])
+
+    # Create the model
+    model = tf.keras.models.Sequential([
+      normalization_layer,
+      data_augmentation, # inactive at test time
+      tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+      tf.keras.layers.MaxPooling2D(2, 2),
+      tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+      tf.keras.layers.MaxPooling2D(2,2),
+      tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+      tf.keras.layers.MaxPooling2D(2,2),
+      tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+      tf.keras.layers.MaxPooling2D(2,2),
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(512, activation='relu'),
+      tf.keras.layers.Dense(151)
+    ])
+
+
+
+    # Compile the model
+    model.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy', precision, recall, f1_score])
+    # Train the model
+    epochs = 2
+    history = model.fit(train_ds,
+                    epochs=epochs,
                     verbose=1,
-                    validation_data=validation_generator)
+                    validation_data=val_ds)
+
+    
+
+
+
+mlflow.end_run()
+
+
+# acc=history.history['accuracy']
+# val_acc=history.history['val_accuracy']
+# loss=history.history['loss']
+# val_loss=history.history['val_loss']
+# precision = history.history['precision']
+# recall = history.history['recall']
+# auc = history.history['auc']
+
+# epochs=range(len(acc))
+# # Plot training and validation accuracy per epoch
+# plt.plot(epochs, acc, 'r', "Training Accuracy")
+# plt.plot(epochs, val_acc, 'b', "Validation Accuracy")
+# plt.title('Training and validation accuracy')
+# plt.savefig('accuracy.png', dpi=80)
+# plt.close()
+
+# # Plot training and validation loss per epoch
+# plt.plot(epochs, loss, 'r', "Training Loss")
+# plt.plot(epochs, val_loss, 'b', "Validation Loss")
+# plt.title('Training and validation loss')
+# plt.savefig('loss.png', dpi=80)
+# plt.close()
+
+# # Plot precision per epoch
+# plt.plot(epochs, precision, 'r', "Precision")
+# plt.title('Precision')
+# plt.savefig('precision.png', dpi=80)
+# plt.close()
+
+# # Plot recall per epoch
+# plt.plot(epochs, recall, 'r', "Recall")
+# plt.title('Recall')
+# plt.savefig('recall.png', dpi=80)
+# plt.close()
+
+# # Plot AUC per epoch
+# plt.plot(epochs, auc, 'r', "AUC")
+# plt.title('AUC')
+# plt.savefig('auc.png', dpi=80)
+# plt.close()
+
+
