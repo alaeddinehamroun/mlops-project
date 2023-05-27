cdbd628ff3b84658602ece20cf303335d59a46c0
alaeddinehamroun <alaeddinehamroun@insat.u-carthage.tn>
2023-05-27 14:54:44 +0100

    added hydra for hyperparams tuning

--- src/train.py
+++ src/train.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import mlflow
 import mlflow
 import tensorflow as tf
-
+import hydra
+from omegaconf import DictConfig
 from metrics import precision, recall, f1_score
 from dotenv import load_dotenv
 load_dotenv()
@@ -22,83 +23,90 @@ img_height = 180
 img_width = 180
 
 
+@hydra.main(version_base=None, config_path='../conf', config_name="config")
 
-# Load dataset
-train_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="training",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
+def main(cfg):
+    
+    # Load dataset
+    train_ds = tf.keras.utils.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="training",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
 
-val_ds = tf.keras.utils.image_dataset_from_directory(
-    data_dir,
-    validation_split=0.2,
-    subset="validation",
-    seed=123,
-    image_size=(img_height, img_width),
-    batch_size=batch_size)
-
-
-# Get number of classes
-num_classes = len(train_ds.class_names)
-
-# Configure the dataset for performance
-# AUTOTUNE = tf.data.AUTOTUNE
-# train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-# val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)    
-
-# Data preprocessing
-
-# Normalization
-normalization_layer = tf.keras.layers.Rescaling(1./255)
-
-# 
+    val_ds = tf.keras.utils.image_dataset_from_directory(
+        data_dir,
+        validation_split=0.2,
+        subset="validation",
+        seed=123,
+        image_size=(img_height, img_width),
+        batch_size=batch_size)
 
 
-# Data augmentation
-data_augmentation = tf.keras.Sequential([
-    tf.keras.layers.RandomFlip("horizontal_and_vertical"),
-    tf.keras.layers.RandomRotation(0.2),
-])
+    # Get number of classes
+    num_classes = len(train_ds.class_names)
 
-# Create the model
-model = tf.keras.models.Sequential([
-    normalization_layer,
-    data_augmentation, # inactive at test time
-    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
-    tf.keras.layers.MaxPooling2D(2,2),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dense(151)
-])
+    # Configure the dataset for performance
+    # AUTOTUNE = tf.data.AUTOTUNE
+    # train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    # val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)    
+
+    # Data preprocessing
+
+    # Normalization
+    normalization_layer = tf.keras.layers.Rescaling(1./255)
+
+    # 
 
 
+    # Data augmentation
+    data_augmentation = tf.keras.Sequential([
+        tf.keras.layers.RandomFlip("horizontal_and_vertical"),
+        tf.keras.layers.RandomRotation(0.2),
+    ])
 
-# Compile the model
-model.compile(
-    optimizer='adam',
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=['accuracy', precision, recall, f1_score])
-# Train the model
-epochs = 2
+    # Create the model
 
-mlflow.tensorflow.autolog()
+    model = tf.keras.models.Sequential([
+        normalization_layer,
+        data_augmentation, # inactive at test time
+        tf.keras.layers.Conv2D(cfg.model.layer1, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(cfg.model.layer2, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+        tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+        tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(151)
+    ])
 
-history = model.fit(train_ds,
-                epochs=epochs,
-                verbose=1,
-                validation_data=val_ds)
+
+
+    # Compile the model
+    model.compile(
+        optimizer='adam',
+        
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy', precision, recall, f1_score])
+    # Train the model
+    epochs = 2
+
+    mlflow.tensorflow.autolog()
+
+    history = model.fit(train_ds,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=val_ds)
 
     
 
+if __name__=="__main__":
+    main()
 
 mlflow.end_run()
 
